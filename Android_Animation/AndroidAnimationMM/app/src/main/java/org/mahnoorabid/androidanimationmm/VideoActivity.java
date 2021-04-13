f22c7487b4051bc6b845250251399cf3738a377c
MahnoorAbid <mcsf19a030@pucit.edu.pk>
2021-04-13 08:10:41 +0500

    first commit

---
+++ Android_Animation/AndroidAnimationMM/app/src/main/java/org/mahnoorabid/androidanimationmm/VideoActivity.java
@@ -0,0 +1,21 @@
+package org.mahnoorabid.androidanimationmm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+        VideoView videoView=findViewById(R.id.videoView);
+        videoView.setVideoPath("android.resource//"+ getPackageName()+ "/" + R.raw.video);
+        MediaController mediaController=new MediaController(this);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+    }
+}
